285dfb8ee86a7af36770e4ba6ed42d11d7742ba3
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2022-04-30 13:58:11 +0200

    Dark-light mode is persistent now

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
-
-const body = document.getElementById("body")
 const sunIcon = document.getElementById("sun");
 const moonIcon = document.getElementById("moon");
 const menuIcon = document.querySelector(".navbar");
-const modeToggler = document.getElementsByClassName("modeToggler");
+const darkModeToggle = document.querySelector(".dark-mode-toggle");
+let darkMode = localStorage.getItem("darkMode");
 
-
-for (i = 0; i < modeToggler.length; i++) {
-    modeToggler[i].addEventListener("click", ()=> {
-        
-        if (body.classList.contains("white") === false) {
-            body.classList.add("white");
-            menuIcon.classList.remove("navbar-dark");
-            menuIcon.classList.add("navbar-light");
-            sunIcon.style.display = "none";
-            moonIcon.style.display = "block";
-            console.log('you')
-        } else {
-            body.classList.remove("white");
-            menuIcon.classList.remove("navbar-light");
-            menuIcon.classList.add("navbar-dark");
-            sunIcon.style.display = "block";
-            moonIcon.style.display = "none";
-            
-        }
-    });
+// Disable dark mode
+const disableDarkMode = () => {
+    document.body.classList.add("white");
+    sunIcon.style.display = "none";
+    moonIcon.style.display = "block";
+    menuIcon.classList.remove("navbar-dark");
+    menuIcon.classList.add("navbar-light");
+    localStorage.setItem("darkMode", "disabled");
 }
+
+// Enable dark mode
+const enableDarkMode = () => {
+    document.body.classList.remove("white");
+    sunIcon.style.display = "block";
+    moonIcon.style.display = "none";
+    menuIcon.classList.remove("navbar-light");
+    menuIcon.classList.add("navbar-dark");
+    localStorage.setItem("darkMode", "enabled");
+}
+
+
+if (darkMode === "disabled") {
+    disableDarkMode();
+}
+
+// Checks if dark mode is disabled or not
+darkModeToggle.addEventListener("click", ()=> {
+    darkMode = localStorage.getItem("darkMode")
+    if (darkMode !== "disabled") {
+        disableDarkMode();
+    } else {
+        enableDarkMode();
+    }
+});
