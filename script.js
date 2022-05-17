9c3259de34db265b65ab19bea1c67247aa90aab4
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2022-05-17 12:49:44 +0200

    Circular values can be displayed dynamically

--- script.js
+++ script.js
@@ -46,3 +46,61 @@ function download(fileUrl, fileName) {
     a.setAttribute("download", fileName);
     a.click();
 }
+
+// Languages circular and progress bar values
+const progressBarCircular1 = document.getElementById("circular1");
+const progressBarCircular2 = document.getElementById("circular2");
+const progressBarCircular3 = document.getElementById("circular3");
+
+const progressValueOne = document.querySelector(".progress-value-one");
+const progressValueTwo = document.querySelector(".progress-value-two");
+const progressValueThree = document.querySelector(".progress-value-three");
+
+let progressValue1 = 0;
+let progressValue2 = 0;
+let progressValue3 = 0;
+
+let progressEndValue1 = 70;
+let progressEndValue2 = 85;
+let progressEndValue3 = 100;
+
+const speed = 20;
+
+// progress circular bar 1
+const progressOne = setInterval(() => {
+    progressValue1++;
+    progressValueOne.textContent = `${progressValue1}%`;
+    progressBarCircular1.style.background = `conic-gradient(
+        #BE2E73 ${progressValue1 * 3.6}deg,
+        #000 ${progressValue1 * 3.6}deg
+    )`;
+    if (progressValue1 === progressEndValue1) {
+        clearInterval(progressOne);
+    }
+}, speed);
+
+// progress circular bar 2
+const progressTwo = setInterval(() => {
+    progressValue2++;
+    progressValueTwo.textContent = `${progressValue2}%`;
+    progressBarCircular2.style.background = `conic-gradient(
+        #BE2E73 ${progressValue2 * 3.6}deg,
+        #000 ${progressValue2 * 3.6}deg
+    )`;
+    if (progressValue2 === progressEndValue2) {
+        clearInterval(progressTwo);
+    }
+}, speed);
+
+// progress circular bar 3
+const progressThree = setInterval(() => {
+    progressValue3++;
+    progressValueThree.textContent = `${progressValue3}%`;
+    progressBarCircular3.style.background = `conic-gradient(
+        #BE2E73 ${progressValue3 * 3.6}deg,
+        #000 ${progressValue3 * 3.6}deg
+    )`;
+    if (progressValue3 === progressEndValue3) {
+        clearInterval(progressThree);
+    }
+}, speed);
