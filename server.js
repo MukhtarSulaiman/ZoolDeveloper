6b9a419a7f188dbe6caed5cb3c10b0965e24a853
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2022-05-30 22:26:37 +0200

    Email authhentication has been changed to OAuth2

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 
 const express = require('express');
 const nodemailer = require('nodemailer');
+const { google } = require('googleapis');
+const { response } = require('express');
 require('dotenv').config({ path: '.env' });
 
 const app = express();
@@ -11,43 +13,65 @@ const PORT = process.env.PORT || 500;
 app.use(express.static('public'));
 
 app.use(express.json());
-app.use(express.urlencoded({
-	extended: false,
+app.use(
+	express.urlencoded({
+		extended: false,
 	})
 );
+
+const OAuth2Client = new google.auth.OAuth2(
+	process.env.CLIENT_ID,
+	process.env.CLIENT_SECRET,
+	process.env.REDIRECT_UTL
+);
+OAuth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/public/index.html');
 });
 
 app.post('/', (req, res) => {
+	 
+	async function sendMail() {
 
-	const transporter = nodemailer.createTransport({
-		service: 'gmail',
-		auth: {
-			user: process.env.EMAIL_USER,
-			pass: process.env.EMAIL_PASSWORD,
-		},
-	});
+		try {
+			const accessToken = await OAuth2Client.getFederatedSignonCerts();
 
-	const mailOption = {
-		from: `'${req.body.fullName}' <${req.body.email}>`,
-		to: process.env.EMAIL_USER,
-		subject: req.body.subject,
-		text: req.body.message,
-	};
-	
-	transporter.sendMail(mailOption, (err, info) => {
-		if (err) {
-			console.log(err);
-			res.status(400).json({ message: err });
-		} else {
-			console.log(`Email sent ${info.response}`);
-			res.status(200).json({
-				message: 'Votre email a été envoyé avec succès',
+			const transporter = nodemailer.createTransport({
+				service: 'gmail',
+				auth: {
+					type: 'OAuth2',
+					user: process.env.SENDER_USER,
+					clientId: process.env.CLIENT_ID,
+					clientSecret: process.env.CLIENT_SECRET,
+					refreshToken: process.env.REFRESH_TOKEN,
+					accessToken: accessToken,
+				},
 			});
-		}
-	});
+
+			const mailOption = {
+				from: `'${req.body.fullName}' <${req.body.email}>`,
+				to: process.env.RECEIVER_USER,
+				subject: req.body.subject,
+				text: req.body.message,
+			};
+
+			const result = await transporter.sendMail(mailOption);
+			return result;
+			
+		} catch (err) {
+			return err;
+		}		 		
+	}
+	sendMail()
+		.then(result => {
+		res.status(200).json({message: 'Votre email a été envoyé avec succès'});
+		console.log(`Email sent ${result}`);
+		})
+		.catch(err => {
+		console.log(err);
+		res.status(400).json({ message: err });
+	})
 });
 
 app.listen(PORT, () => {
